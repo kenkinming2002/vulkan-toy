5c1bd59113ffe80b6b6a9ab01f654433528a090e
KenKwok <kenkinming2002@gmail.com>
2022-08-31 20:38:28 +0800

    Remove render pass abstraction

--- src/render/render_target.cpp
+++ src/render/render_target.cpp
@@ -14,13 +14,14 @@ namespace vulkan
     allocator_t allocator;
     swapchain_t swapchain;
 
-    RenderPass   render_pass;
-    image_t      depth_image;
-    image_view_t depth_image_view;
+    VkRenderPass   render_pass;
+
+    image_t        depth_image;
+    image_view_t   depth_image_view;
     Framebuffer *framebuffers;
 
-    uint32_t image_index;
     uint32_t image_count;
+    uint32_t image_index;
 
     Frame frames[MAX_FRAME_IN_FLIGHT];
     size_t frame_index;
@@ -38,11 +39,67 @@ namespace vulkan
     render_target->allocator = allocator;
     render_target->swapchain = swapchain;
 
+    VkDevice device = context_get_device_handle(render_target->context);
+
     // Render pass
-    init_render_pass_simple(render_target->context, RenderPassCreateInfoSimple{
-      .color_format = swapchain_get_format(render_target->swapchain),
-      .depth_format = VK_FORMAT_D32_SFLOAT,
-    }, render_target->render_pass);
+    VkAttachmentDescription color_attachment_description = {};
+    color_attachment_description.format         = swapchain_get_format(render_target->swapchain);
+    color_attachment_description.samples        = VK_SAMPLE_COUNT_1_BIT;
+    color_attachment_description.loadOp         = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    color_attachment_description.storeOp        = VK_ATTACHMENT_STORE_OP_STORE;
+    color_attachment_description.stencilLoadOp  = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    color_attachment_description.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    color_attachment_description.initialLayout  = VK_IMAGE_LAYOUT_UNDEFINED;
+    color_attachment_description.finalLayout    = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentDescription depth_attachment_description = {};
+    depth_attachment_description.format         = VK_FORMAT_D32_SFLOAT;
+    depth_attachment_description.samples        = VK_SAMPLE_COUNT_1_BIT;
+    depth_attachment_description.loadOp         = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    depth_attachment_description.storeOp        = VK_ATTACHMENT_STORE_OP_STORE;
+    depth_attachment_description.stencilLoadOp  = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    depth_attachment_description.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    depth_attachment_description.initialLayout  = VK_IMAGE_LAYOUT_UNDEFINED;
+    depth_attachment_description.finalLayout    = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+    VkAttachmentDescription attachment_descriptions[] = {
+      color_attachment_description,
+      depth_attachment_description,
+    };
+
+    VkAttachmentReference color_attachment_reference = {};
+    color_attachment_reference.attachment = 0;
+    color_attachment_reference.layout     = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkAttachmentReference depth_attachment_reference = {};
+    depth_attachment_reference.attachment = 1;
+    depth_attachment_reference.layout     = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass_description = {};
+    subpass_description.pipelineBindPoint       = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass_description.colorAttachmentCount    = 1;
+    subpass_description.pColorAttachments       = &color_attachment_reference;
+    subpass_description.pDepthStencilAttachment = &depth_attachment_reference;
+
+    VkSubpassDependency subpass_dependency = {};
+    subpass_dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+    subpass_dependency.dstSubpass = 0;
+    subpass_dependency.srcStageMask  = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+    subpass_dependency.srcAccessMask = 0;
+    subpass_dependency.dstStageMask  = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+    subpass_dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+
+
+    VkRenderPassCreateInfo render_pass_create_info = {};
+    render_pass_create_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    render_pass_create_info.attachmentCount = std::size(attachment_descriptions);
+    render_pass_create_info.pAttachments    = attachment_descriptions;
+    render_pass_create_info.subpassCount    = 1;
+    render_pass_create_info.pSubpasses      = &subpass_description;
+    render_pass_create_info.dependencyCount = 1;
+    render_pass_create_info.pDependencies   = &subpass_dependency;
+
+    VK_CHECK(vkCreateRenderPass(device, &render_pass_create_info, nullptr, &render_target->render_pass));
 
     // Depth attachment
     VkExtent2D extent = swapchain_get_extent(render_target->swapchain);
@@ -71,7 +128,7 @@ namespace vulkan
         render_target->depth_image_view,
       };
       init_framebuffer(render_target->context, FramebufferCreateInfo{
-        .render_pass      = render_target->render_pass.handle,
+        .render_pass      = render_target->render_pass,
         .extent           = swapchain_get_extent(render_target->swapchain),
         .image_views      = image_views,
         .image_view_count = std::size(image_views),
@@ -92,6 +149,8 @@ namespace vulkan
 
   void render_target_destroy(render_target_t render_target)
   {
+    VkDevice device = context_get_device_handle(render_target->context);
+
     for(uint32_t i=0; i<render_target->image_count; ++i)
       deinit_framebuffer(render_target->context, render_target->framebuffers[i]);
 
@@ -103,7 +162,7 @@ namespace vulkan
     for(size_t i=0; i<MAX_FRAME_IN_FLIGHT; ++i)
       frame_deinit(render_target->context, render_target->frames[i]);
 
-    deinit_render_pass(render_target->context, render_target->render_pass);
+    vkDestroyRenderPass(device, render_target->render_pass, nullptr);
 
     put(render_target->swapchain);
     put(render_target->context);
@@ -129,7 +188,7 @@ namespace vulkan
 
     VkRenderPassBeginInfo render_pass_begin_info = {};
     render_pass_begin_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-    render_pass_begin_info.renderPass        = render_target->render_pass.handle;
+    render_pass_begin_info.renderPass        = render_target->render_pass;
     render_pass_begin_info.framebuffer       = render_target->framebuffers[render_target->image_index].handle;
     render_pass_begin_info.renderArea.offset = {0, 0};
     render_pass_begin_info.renderArea.extent = swapchain_get_extent(render_target->swapchain);
@@ -161,7 +220,7 @@ namespace vulkan
 
   VkRenderPass render_target_get_render_pass(render_target_t render_target)
   {
-    return render_target->render_pass.handle;
+    return render_target->render_pass;
   }
 
   void render_target_get_extent(render_target_t render_target, unsigned& width, unsigned& height)
